9430536cc9bd547f78d84e30a5876736d68f9ddd
Tijmen de Mes <tijmen@ag-projects.com>
2023-01-20 07:52:16 +0000

    Raised version and updated changelog

--- application/__info__.py
+++ application/__info__.py
@@ -5,10 +5,10 @@ __project__ = 'python3-application'
 __summary__ = 'Basic building blocks for python3 applications'
 __webpage__ = 'https://github.com/AGProjects/python3-application'
 
-__version__ = '3.0.4'
+__version__ = '3.0.5'
 
 __author__ = 'Dan Pascu'
 __email__ = 'support@ag-projects.com'
 
 __license__ = 'LGPL'
-__copyright__ = 'Copyright 2022 {}'.format(__author__)
+__copyright__ = 'Copyright 2023 {}'.format(__author__)
